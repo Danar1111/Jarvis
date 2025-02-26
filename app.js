66b9b109eac6c3bef50708d71277d71596698fff
Danar <114123567+Danar1111@users.noreply.github.com>
2025-02-26 23:35:55 +0700

    Using /api for routes

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(logger);
 
-// app.use('/api', apiRoutes);
+app.use('/api', apiRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running on PORT ${PORT}`);
